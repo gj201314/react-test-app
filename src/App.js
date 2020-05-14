c1cc2a0626f7e4f814a3da379e69c3fb0390b59c
guojie <1580911511@qq.com>
2020-05-14 17:33:25 +0800

    state和props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Square extends React.Component {
+	render(){
+		return (
+			<button type="button" onClick={()=> this.props.onClick()}>{this.props.value}</button>
+		)
+	}
+}
+
+class App extends React.Component {
+  constructor() {
+      super();
+	  this.state = {
+		  squares:[0,1,2,3,4,5,6]
+	  }
+  }
+  handleClick(i){
+	  let arr = [...this.state.squares];
+	  arr[i]='X';
+	  this.setState({
+		  squares:arr
+	  })
+  }
+  renderSquare(i){
+	  return (
+		<Square value={this.state.squares[i]} index="index" onClick={()=>this.handleClick(i)} />
+	  )
+  }
+  render(){
+	  return (
+	    <div className="App">
+	      <h1>{this.props.name}</h1>
+		  <div className="broad">
+			{this.renderSquare(0)}
+			{this.renderSquare(1)}
+			{this.renderSquare(2)}
+		  </div>
+		  <div className="broad">
+			{this.renderSquare(4)}
+			{this.renderSquare(5)}
+			{this.renderSquare(6)}
+		  </div>
+	    </div>
+	  );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="react-app" />
   </React.StrictMode>,
   document.getElementById('root')
 );
